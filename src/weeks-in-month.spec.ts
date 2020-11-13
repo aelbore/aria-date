f091612567bdd2a92f420b80572f7c8b4865322a
aelbore <aelbore.sg@gmail.com>
2020-11-13 23:17:24 +0800

    fixed code, add tests

--- src/weeks-in-month.spec.ts
+++ src/weeks-in-month.spec.ts
@@ -1,9 +1,66 @@
 import { expect } from 'aria-mocha'
+import { WeekRange } from './types'
 import { getWeeksInMonth } from './weeks-in-month'
 
 describe('getWeeksInMonth', () => {
 
-  it('should get the weeks range in the given month & year', () => {
+  const asserts = (results: WeekRange[], expected: WeekRange[]) => {
+    expect(results.length).equal(expected.length)
+    results.forEach((result, index) => {
+      expect(result.start).equal(expected[index].start)
+      expect(result.end).equal(expected[index].end)
+    })
+  }
+
+  it('should get the weeks range when Month is January 2020 with 31 days', () => {
+    const date = new Date('14-Jan-2020')
+
+    const expected: WeekRange[] = [
+      { start: 1, end: 5 },
+      { start: 6, end: 12 },
+      { start: 13, end: 19 },
+      { start: 20, end: 26 },
+      { start: 27, end: 31 }
+    ]
+
+    const results = getWeeksInMonth(date)
+
+    asserts(results, expected)
+  })
+
+  it('should get the weeks range when Month is Feb 2020 with 29 days', () => {
+    const date = new Date('01-Feb-2020')
+
+    const expected = [
+      { start: 1, end: 2 },
+      { start: 3, end: 9 },
+      { start: 10, end: 16 },
+      { start: 17, end: 23 },
+      { start: 24, end: 29 }
+    ]
+
+    const results = getWeeksInMonth(date)
+
+    asserts(results, expected)
+  })
+
+  it('should get the weeks range when Month is Feb 2017 with 28 days', () => {
+    const date = new Date('10-Feb-2017')
+
+    const expected = [
+      { start: 1, end: 5 },
+      { start: 6, end: 12 },
+      { start: 13, end: 19 },
+      { start: 20, end: 26 },
+      { start: 27, end: 28 }
+    ]
+
+    const results = getWeeksInMonth(date)
+
+    asserts(results, expected)
+  })
+
+  it('should get the weeks range when Month is Nov 2020 with 30 days', () => {
     const date = new Date('12-Nov-2020')
 
     const expected = [
@@ -17,11 +74,7 @@ describe('getWeeksInMonth', () => {
 
     const results = getWeeksInMonth(date)
     
-    expect(results.length).equal(expected.length)
-    results.forEach((result, index) => {
-      expect(result.start).equal(expected[index].start)
-      expect(result.end).equal(expected[index].end)
-    })
+    asserts(results, expected)
   })
 
 })
--- src/weeks-in-month.ts
+++ src/weeks-in-month.ts
@@ -1,30 +1,21 @@
 import { WeekRange } from './types'
 
 export function getWeeksInMonth(date: Date) {
-  const month  = date.getMonth(), year = date.getFullYear(), _start = 'monday'
+  const month  = date.getMonth(), year = date.getFullYear()
 
   const weeks: WeekRange[] = []
   const firstDate = new Date(year, month, 1)
   const lastDate = new Date(year, month + 1, 0)
   const numDays = lastDate.getDate()
 
-  let start = 1, end = 7 - firstDate.getDay()
-  
-  if (_start === 'monday') {
-    if (firstDate.getDay() === 0) {
-      end = 1
-    } else {
-      end = 7 - firstDate.getDay() + 1
-    }
-  }
+  let start = 1, end = (firstDate.getDay() === 0) ? 1: 7 - firstDate.getDay() + 1
 
   while (start <= numDays) {
-    weeks.push({ start: start, end: end })
+    weeks.push({ start, end })
     
     start = end + 1
     end = end + 7
-    end = start === 1 && end === 8 ? 1 : end
-    
+
     if (end > numDays) {
       end = numDays
     }
